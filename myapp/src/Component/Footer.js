970ae1872dc0eec53b00de377852a3fac11a9b4a
amandeepsingh360 <98327717+amandeepsingh360@users.noreply.github.com>
2023-05-20 11:45:19 +0530

    Update Footer.js

--- myapp/src/Component/Footer.js
+++ myapp/src/Component/Footer.js
@@ -2,14 +2,11 @@ import React from 'react'
 import discord from './discord.png'
 export default function Footer() {
     return (
-        <div className='mt-3 flex-col justify-end bg-blue-800 text-white p-3 font-extrabold text-6xl'>
+        <div className='mt-3 flex-col justify-end bg-blue-800 text-white p-1 font-extrabold text-3xl'>
             <div className='flex justify-center items-center'>
-                Join our community 
-            </div>
-            <div className='flex justify-center items-center'>
-                of active users on
+                Join our community of active users on
                 <button className='ml-3 mt-3' type='button' onClick={()=>{}}>
-                    <img className='rounded-lg' width={"70px"} src={discord} alt="" />
+                    <img className='rounded-lg' width={"50px"} src={discord} alt="" />
                 </button>
             </div>
         </div>
