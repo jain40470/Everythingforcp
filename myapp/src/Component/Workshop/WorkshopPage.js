bdf5bcd874a1b84f325ba4c61533fff1e157b069
amandeepsingh360 <98327717+amandeepsingh360@users.noreply.github.com>
2023-05-21 12:12:04 +0530

    Update WorkshopPage.js

--- myapp/src/Component/Workshop/WorkshopPage.js
+++ myapp/src/Component/Workshop/WorkshopPage.js
@@ -6,10 +6,10 @@ export default function WorkshopPage() {
   return (
     <div className="mb-20">
       <div className="flex place-content-evenly pt-20 mb-40">
-        {<WorkCard date="2024-05-20" time="11:22" />}
-        {<WorkCard date="2025-05-01" time="23:43" />}
-        {<WorkCard date="2027-11-15" time="01:17" />}
-        {<WorkCard date="2029-13-22" time="11:13" />}
+        {<WorkCard userHandle="amandeep360" date="2024-05-20" time="11:22"/>}
+        {<WorkCard userHandle="jain40470" date="2025-05-01" time="23:43"/>}
+        {<WorkCard userHandle="12_Utpal" date="2027-11-15" time="01:17"/>}
+        {<WorkCard userHandle="asishkumar" date="2029-13-22" time="11:13"/>}
       </div>
       <WorkshopD />
     </div>
