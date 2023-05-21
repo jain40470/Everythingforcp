b35ee8d8b59fc6a192fe9fd818a6e3de5a2f0020
amandeepsingh360 <98327717+amandeepsingh360@users.noreply.github.com>
2023-05-21 12:14:28 +0530

    Add files via upload

---
+++ myapp/src/Component/About/CardTeam.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Card(props) {
+    return (
+        <div className='flex justify-center my-56'>
+            <div className="flex flex-col items-center rounded-lg shadow md:flex-row md:max-w-xl dark:bg-blue-900">
+                <div className="flex flex-col justify-between p-4 leading-normal">
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Name : {props.name}</h5>
+                    <p className="font-normal text-2xl text-white">CF ID : <a href={`https://codeforces.com/profile/${props.CF_id}`} target='_blank' rel='noreferrer'>{props.CF_id}</a></p>
+                </div>
+            </div>
+        </div>
+    )
+}
