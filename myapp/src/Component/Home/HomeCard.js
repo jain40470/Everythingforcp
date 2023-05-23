2c56cfab4fff023f2062a74d2595c4793ace9a40
amandeepsingh360 <98327717+amandeepsingh360@users.noreply.github.com>
2023-05-23 11:59:10 +0530

    Add files via upload

---
+++ myapp/src/Component/Home/HomeCard.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function HomeCard(props) {
+    return (
+        <div className="bg-blue-200 text-blue-900 rounded-lg p-4 w-48 gap-5 flex flex-col items-center">
+            <div className="h-40 w-full flex justify-center items-center">
+                <img
+                    className="w-full h-full rounded-md"
+                    src={props.photo}
+                    alt="Image"
+                />
+            </div>
+
+            <a
+                href={props.link}
+                target="_blank"
+                rel="noreferrer"
+            >
+                {props.name}
+            </a>
+        </div>
+    )
+}
