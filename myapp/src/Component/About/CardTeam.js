af951ddea1cd68f153f40283a77c53636758dc0e
amandeepsingh360 <98327717+amandeepsingh360@users.noreply.github.com>
2023-05-23 10:45:10 +0530

    Update CardTeam.js

--- myapp/src/Component/About/CardTeam.js
+++ myapp/src/Component/About/CardTeam.js
@@ -2,12 +2,10 @@ import React from "react";
 
 export default function Card(props) {
   return (
-    <div className="flex justify-center my-56">
-      <div className="flex flex-col items-center rounded-lg shadow md:flex-row md:max-w-xl dark:bg-blue-900">
-        <div className="flex flex-col justify-between p-4 leading-normal">
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            Name : {props.name}
-          </h5>
+    <div className='flex justify-center mt-44'>
+      <div className="flex flex-col flex-wrap items-center rounded-lg shadow md:flex-row dark:bg-blue-900">
+        <div className="flex-col p-4">
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-white">Name : {props.name}</h5>
           <p className="font-normal text-2xl text-white">
             CF ID :{" "}
             <a
