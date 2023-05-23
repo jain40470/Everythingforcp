7a7bd22ec867ef82a11e709bac69520edfa50323
amandeepsingh360 <98327717+amandeepsingh360@users.noreply.github.com>
2023-05-24 00:58:06 +0530

    Update Team.js

--- myapp/src/Component/About/Team.js
+++ myapp/src/Component/About/Team.js
@@ -1,11 +1,12 @@
-import React from "react";
-import CardTeam from "./CardTeam";
-
+import React from 'react'
+import CardTeam from './CardTeam'
+import aman from './Assets/aman.jpg'
+import akash from './Assets/akash.jpg'
 export default function Team() {
   return (
     <div className='flex flex-wrap justify-evenly'>
-      <CardTeam name="Akash Jain" CF_id="jain40470" />
-      <CardTeam name="Amandeep Singh" CF_id="amandeep360" />
+        <CardTeam name="Akash Jain" CF_id="jain40470" photo={akash}/>
+        <CardTeam name="Amandeep Singh" CF_id="amandeep360" photo={aman}/>
     </div>
-  );
+  )
 }
